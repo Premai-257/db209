5cff799526e2be897985a4d355b8f5133f916b61
Premai-257 <65111257@dpu.ac.th>
2022-10-28 12:08:35 +0700

    lab6

---
+++ Lab5-rps/js/app.js
@@ -0,0 +1,100 @@
+let userScore = 0;
+let compScore = 0;
+
+const USERSCORE = document.querySelector('#user-score');
+const COMPSCORE = document.querySelector('#comp-score');
+const RESULT = document.querySelector('#result')
+const USERCHOICE = document.querySelector('#user-choice');
+const COMPCHOICE = document.querySelector('#comp-choice');
+const CHOICE = document.querySelectorAll('.choice');
+const MESSAGE = document.querySelector('#measage');
+
+function ready () {
+    USERCHOICE.setAttribute('src', `image/rock.png`);
+    COMPCHOICE.setAttribute('src', `image/rock.png`);
+
+    USERCHOICE.classList.add('ready');
+    COMPCHOICE.classList.add('ready');
+}
+
+for(let choice of CHOICE) {
+    choice.addEventListener('click', function() {
+        play(this.id);
+    });
+}
+ready();
+
+function computerSelect() {
+    let choices = ['rock', 'paper', 'scissors'];
+    let selectedIndex = Math.floor(Math.random()*3);
+    return choices[selectedIndex];
+}
+
+function play(userChoice) {
+    USERCHOICE.classList.remove('ready');
+    COMPCHOICE.classList.remove('ready');
+
+   let compChoice = computerSelect();
+   
+   USERCHOICE.setAttribute('src', `image/${userChoice}.png`);
+   COMPCHOICE.setAttribute('src', `image/${compChoice}.png`);
+//    if(
+//       (userChoice == 'rock' && compChoice == 'scissors') || 
+//       (userChoice == 'paper' && compChoice == 'rock')  ||
+//       (userChoice == 'scissors' && compChoice == 'paper') 
+//     ) {
+//       win(userChoice, compChoice);
+//    } 
+//    else if(
+//       (userChoice == 'rock' && compChoice == 'paper') || 
+//       (userChoice == 'paper' && compChoice == 'scissors')  ||
+//       (userChoice == 'scissors' && compChoice == 'rock')
+//    ) {
+//      lose(userChoice, compChoice);
+//    }
+//      else{
+//      draw(userChoice);
+//    }
+
+  let battle = userChoice+compChoice;
+  if(
+          (battle == 'rockscissors') || 
+          (battle == 'paperrock')  ||
+          (battle == 'scissorspaper') 
+        ) {
+          win(userChoice, compChoice);
+       } 
+       else if(
+          (battle == 'rockpaper') || 
+          (battle == 'paperscissors')  ||
+          (battle == 'scissorsrock')
+       ) {
+         lose(userChoice, compChoice);
+       }
+         else{
+         draw(userChoice);
+       }
+       setTimeout(()=>{
+        ready();
+       }, 350);
+}   
+
+function win(user, comp) {
+    USERSCORE.innerHTML = ++userScore;
+    RESULT.innerHTML = `${user.toUpperCase()} beats `;
+    RESULT.innerHTML += `${comp.toUpperCase()}. `;
+    RESULT.innerHTML += `You <i>win!</i>`;
+}
+
+function lose(user, comp) {
+    COMPSCORE.innerHTML = ++compScore;
+    RESULT.innerHTML = `${comp.toUpperCase()} beats `;
+    RESULT.innerHTML += `${user.toUpperCase()}. `;
+    RESULT.innerHTML += `You <i>lose!</i>`;
+}
+
+function draw(user) {
+    USERSCORE.innerHTML = ++userScore;
+    RESULT.innerHTML = `It was draw! You both chose `; 
+    RESULT.innerHTML += `${user.toUpperCase()}.`;
+}
